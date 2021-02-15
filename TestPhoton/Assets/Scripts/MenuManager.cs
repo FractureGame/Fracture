00c12577049179799a9d60ff984c85a14c826094
RedBetta <61059921+RedBetta@users.noreply.github.com>
2021-02-15 18:10:35 +0100

    testPhoton

---
+++ TestPhoton/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using ExitGames.Client.Photon.StructWrapping;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = System.Random;
+
+
+namespace Com.MyCompany.MyGame
+{
+    public class MenuManager : MonoBehaviourPunCallbacks, IMatchmakingCallbacks
+    {
+        #region Private Serializable Fields
+        [Tooltip("The maximum number of players per room. When a room is full, it can't be joined by new players, and so new room will be created")]
+        [SerializeField]
+        private byte maxPlayersPerRoom = 2;
+
+        #endregion
+
+
+        #region Private Fields
+
+        
+        /// <summary>
+        /// Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photon,
+        /// we need to keep track of this to properly adjust the behavior when we receive call back by Photon.
+        /// Typically this is used for the OnConnectedToMaster() callback.
+        /// </summary>
+        bool isConnecting;
+        private bool publicGame;
+        private string value;
+        private bool creator;
+        private const int CODE_LENGTH = 4;
+        private string code;
+
+        #endregion
+
+
+        #region Public Fields
+        
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        [SerializeField]
+        private GameObject progressLabel;
+        
+        [Tooltip("The Play Menu")]
+        [SerializeField]
+        private GameObject menuPlay;
+
+        [Tooltip("Invite Code Label")]
+        [SerializeField]
+        private GameObject inviteCodeLabel;
+
+        [Tooltip("input Room Name To Join")]
+        [SerializeField]
+        private GameObject inputRoomNameToJoin;
+        
+        [Tooltip("Erro Join Label")]
+        [SerializeField]
+        private GameObject errorJoinLabel;
+        
+        [Tooltip("Join a Private Room menu")]
+        [SerializeField]
+        private GameObject joinPrivateGameMenu;
+        
+        [Tooltip("LaunchGame Button")]
+        [SerializeField]
+        private GameObject launchGameButton;
+        
+        [Tooltip("Player2 Label")]
+        [SerializeField]
+        private GameObject player2Label;
+        
+        [Tooltip("Master Label")]
+        [SerializeField]
+        private GameObject masterLabel;
+        
+        [Tooltip("Back Button")]
+        [SerializeField]
+        private GameObject backButton;
+        
+        #endregion
+        
+        #region MonoBehaviour CallBacks
+
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        /// </summary>
+        void Awake()
+        {
+            // #Critical
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+
+        /// <summary>
+        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+        /// </summary>
+        void Start()
+        {
+            progressLabel.SetActive(false);
+            menuPlay.SetActive(true);
+            backButton.SetActive(true);
+            masterLabel.SetActive(false);
+            player2Label.SetActive(false);
+            launchGameButton.SetActive(false);
+            joinPrivateGameMenu.SetActive(false);
+            inviteCodeLabel.SetActive(false);
+            
+        }
+
+
+        #endregion
+
+
+        #region Public Methods
+
+        public void PlayOnline()
+        {
+            Debug.Log("PLAYONLINE");
+            publicGame = true;
+            menuPlay.SetActive(false);
+            progressLabel.SetActive(true);
+            backButton.GetComponentInChildren<Text>().text = "Quit Queue";
+            PhotonNetwork.JoinRandomRoom();
+        }
+
+        public string GenerateRandomCode()
+        {
+            string code = "";
+            Random rand = new Random();
+            for (int i = 0; i < CODE_LENGTH; i++)
+            {
+                code += (char) rand.Next(65, 91);
+            }
+
+            return code;
+        }
+
+        public void CreatePrivateRoom()
+        {
+            publicGame = false;
+            creator = true;
+            backButton.GetComponentInChildren<Text>().text = "Quit Room";
+            menuPlay.SetActive(false);
+            masterLabel.SetActive(true);
+            player2Label.SetActive(true);
+            masterLabel.GetComponent<Text>().text = PhotonNetwork.NickName;
+            string code = GenerateRandomCode();
+            PhotonNetwork.CreateRoom(code, new RoomOptions {MaxPlayers = maxPlayersPerRoom, IsVisible = false}, null,
+                new string[]{});
+        }
+
+        
+        public void DisplayJoinPrivateRoom()
+        {
+            menuPlay.SetActive(false);
+            joinPrivateGameMenu.SetActive(true);
+        }
+        
+        public void JoinPrivateRoom()
+        {
+            errorJoinLabel.GetComponent<Text>().text = "";
+            creator = false;
+            publicGame = false;
+            backButton.GetComponentInChildren<Text>().text = "Quit Room";
+            value = inputRoomNameToJoin.GetComponent<Text>().text;
+            if (string.IsNullOrEmpty(value))
+            {
+                Debug.LogError("Room Name is null or empty");
+                errorJoinLabel.GetComponent<Text>().text = "Room Name is null or empty";
+                
+                return;
+            }
+            PhotonNetwork.JoinRoom(value);
+        }
+
+        public void LaunchGame()
+        {
+            Debug.Log("We load the 'MapGame");
+            PhotonNetwork.LoadLevel("MapGame");
+        }
+
+
+        public void Back()
+        {
+            if (menuPlay.activeSelf)
+            {
+                SceneManager.LoadScene("Launcher");
+            }
+            else if(joinPrivateGameMenu.activeSelf)
+            {
+                joinPrivateGameMenu.SetActive(false);
+                menuPlay.SetActive(true);
+                errorJoinLabel.GetComponent<Text>().text = "";
+            }
+            else if (inviteCodeLabel.activeSelf)
+            {
+                PhotonNetwork.LeaveRoom();
+                backButton.GetComponentInChildren<Text>().text = "Back";
+                inviteCodeLabel.SetActive(false);
+                masterLabel.SetActive(false);
+                player2Label.SetActive(false);
+                launchGameButton.SetActive(false);
+                menuPlay.SetActive(true);
+            }
+            else if (player2Label.activeSelf && !creator)
+            {
+                PhotonNetwork.LeaveRoom();
+                backButton.GetComponentInChildren<Text>().text = "Back";
+                masterLabel.SetActive(false);
+                player2Label.SetActive(false);
+                menuPlay.SetActive(true);
+            }
+            else if (publicGame)
+            {
+                PhotonNetwork.LeaveRoom();
+                backButton.GetComponentInChildren<Text>().text = "Back";
+                progressLabel.SetActive(false);
+                menuPlay.SetActive(true);
+            }
+        }
+        
+        #endregion
+    
+        #region MonoBehaviourPunCallbacks Callbacks
+        
+        // public override void OnConnectedToMaster()
+        // {
+        //     DisplayPlayMenu();
+        // }
+
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            isConnecting = false;
+            progressLabel.SetActive(false);
+            // controlPanel.SetActive(true);
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
+
+        public override void OnCreateRoomFailed(short returnCode, string message)
+        {
+            // errorCreateLabel.GetComponentInChildren<Text>().text = "Error: Room already exists";
+            code = GenerateRandomCode();
+            PhotonNetwork.CreateRoom(code, new RoomOptions {MaxPlayers = maxPlayersPerRoom}, null,
+                new string[]{});
+        }
+        
+        public override void OnCreatedRoom()
+        {
+            Debug.Log("ONCREATEDROOM");
+            if (PhotonNetwork.IsMasterClient && !publicGame)
+            {
+                inviteCodeLabel.SetActive(true);
+                inviteCodeLabel.GetComponent<Text>().text = "CODE : " + PhotonNetwork.CurrentRoom.Name;
+            }
+        }
+
+        public override void OnJoinRoomFailed(short returnCode, string message)
+        {
+            Debug.Log("OnJoinRoomFailed");
+            if (!publicGame)
+            {
+                // GameObject rr = joinPrivateGameMenu.transform.Find("Error Join Label").gameObject;
+                // rr.GetComponent<Text>().text = "Error: Room does not exist";
+                errorJoinLabel.GetComponent<Text>().text = "Error: Room does not exist";
+                DisplayJoinPrivateRoom();
+                
+            }
+        }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+            
+            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+            if (publicGame)
+            {
+                PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+            }
+        }
+        
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+            if (PhotonNetwork.IsMasterClient && publicGame)
+            {
+                progressLabel.GetComponent<Text>().text = "Connected. Waiting for my special someone...";
+            }
+            else if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                menuPlay.SetActive(false);
+                masterLabel.SetActive(true);
+                player2Label.SetActive(true);
+                launchGameButton.SetActive(false);
+                masterLabel.GetComponent<Text>().text = PhotonNetwork.NickName;
+            }
+            else if (PhotonNetwork.CurrentRoom.PlayerCount == 2 && !publicGame)
+            {
+                menuPlay.SetActive(false);
+                joinPrivateGameMenu.SetActive(false);
+                masterLabel.SetActive(true);
+                player2Label.SetActive(true);
+                masterLabel.GetComponent<Text>().text = PhotonNetwork.MasterClient.NickName;
+                player2Label.GetComponent<Text>().text = PhotonNetwork.NickName;
+            }
+        }
+
+        public override void OnPlayerEnteredRoom(Player newPlayer)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnPlayerEnteredRoom() called by PUN. Now this client is in a room.");
+            if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 2 && publicGame)
+            {
+                Debug.Log("We load the 'MapGame");
+                PhotonNetwork.LoadLevel("MapGame");
+            }
+            else if (PhotonNetwork.IsMasterClient && PhotonNetwork.CurrentRoom.PlayerCount == 2)
+            {
+                player2Label.GetComponentInChildren<Text>().text = PhotonNetwork.PlayerList[1].NickName;
+                launchGameButton.SetActive(true);
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            if (PhotonNetwork.CurrentRoom.PlayerCount == 1 && !creator)
+            {
+                creator = true;
+                masterLabel.GetComponent<Text>().text = PhotonNetwork.MasterClient.NickName;
+                player2Label.GetComponent<Text>().text = "Waiting for my special someone...";
+                inviteCodeLabel.GetComponent<Text>().text = "CODE : " + PhotonNetwork.CurrentRoom.Name;
+                inviteCodeLabel.SetActive(true);
+            }
+            else if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+            {
+                player2Label.GetComponent<Text>().text = "Waiting for my special someone...";
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ TestPhoton/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerMovement : MonoBehaviourPunCallbacks
+{
+
+    private float x;
+    public float speed;
+    private Rigidbody2D rigidbody;
+    public float jumpForce;
+    private bool jump;
+
+    private Vector3 movement;
+    // [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+    // public static GameObject LocalPlayerInstance;
+
+    // private void Awake()
+    // {
+    //     if (photonView.IsMine)
+    //     {
+    //         PlayerMovement.LocalPlayerInstance = this.gameObject;
+    //     }
+    //     DontDestroyOnLoad(this.gameObject);
+    // }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        // if (PhotonNetwork.IsMasterClient)
+        // {
+        //     transform.position = new Vector2(0f, 5f);
+        // }
+        // else
+        // {
+        //     transform.position = new Vector2(0f, -5f);
+        // }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected)
+        {
+            return;
+        }
+        x = Input.GetAxis("Horizontal"); // left = -1, right = 1
+        jump = Input.GetButtonDown("Jump");
+        // if (x > 0)
+        // {
+        //     transform.Translate(transform.forward * Time.deltaTime * speed, Space.World);
+        // }
+        // else if (x < 0)
+        // {
+        //     transform.Translate(-transform.forward * Time.deltaTime * speed, Space.World);
+        // }
+        // transform.position += (Vector3) new Vector2(x*speed*Time.deltaTime, 0);
+        // transform.position += new Vector3(x*speed*Time.deltaTime, 0);
+        //Move Front/Back
+        // if (Input.GetKey(KeyCode.Z))
+        // {
+        //     transform.Translate(transform.forward * Time.deltaTime * speed, Space.World);
+        // }
+        // else if (Input.GetKey(KeyCode.S))
+        // {
+        //     transform.Translate(-transform.forward * Time.deltaTime * speed, Space.World);
+        // }
+    }
+
+    private void FixedUpdate()
+    {
+        if (photonView.IsMine == false && PhotonNetwork.IsConnected)
+        {
+            return;
+        }
+        transform.position += new Vector3(x*speed*Time.deltaTime, 0);
+        if (jump)
+        {
+            rigidbody.AddForce(transform.up*jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
