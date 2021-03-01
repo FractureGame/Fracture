7551656e594b6f872ddc3b26b6f1d5ef3df055fa
Shinwoir <raphael.leroy@epita.fr>
2021-03-01 15:30:49 +0100

    hmmmm oui added input field oui hmmmmmmmmm

--- Fracture UI/Assets/Scripts/MainMenu.cs
+++ Fracture UI/Assets/Scripts/MainMenu.cs
@@ -7,11 +7,6 @@ public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public void LaunchGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
     public void QuitGame()
     {
         Application.Quit();
