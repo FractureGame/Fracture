b612a00cdfdb0f52534ef5a20da3d589626396c4
Shinwoir <raphael.leroy@epita.fr>
2021-03-01 16:10:40 +0100

    done with buttons, no functionality yet

--- Fracture UI/Assets/Scripts/MainMenu.cs
+++ Fracture UI/Assets/Scripts/MainMenu.cs
@@ -5,8 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     public void QuitGame()
     {
         Application.Quit();
