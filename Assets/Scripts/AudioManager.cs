1fd94229ad5d7fe13d766705df98cc90f67d45f4
Shinwoir <raphael.leroy@epita.fr>
2021-03-02 18:42:23 +0100

    audio working besides going back to main menu

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        foreach (Sound snd in sounds)
+        {
+            snd.source = gameObject.AddComponent<AudioSource>();
+            snd.source.clip = snd.sound;
+            snd.source.volume = snd.volume;
+            snd.source.pitch = snd.pitch;
+            snd.source.loop = snd.loopable;
+        }
+    }
+
+    // Update is called once per frame
+    public void PlaySound(string name)
+    {
+        Sound snd = Array.Find(sounds, sound => sound.name == name);
+        if (snd == null)
+        {
+            return;
+        }
+        snd.source.Play();
+    }
+
+    public void StopSound(string name)
+    {
+        Sound snd = Array.Find(sounds, sound => sound.name == name);
+        if (snd == null)
+        {
+            return;
+        }
+        snd.source.Stop();
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Com.MyCompany.MyGame;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void Start()
+    {
+        FindObjectOfType<AudioManager>().PlaySound("MenuTheme");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
