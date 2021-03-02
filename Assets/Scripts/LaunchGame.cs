a3fc6a04263c93e44b651111a167f74f1e83a5b8
Shinwoir <raphael.leroy@epita.fr>
2021-03-02 17:53:59 +0100

    repush

---
+++ Assets/Scripts/LaunchGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaunchGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
