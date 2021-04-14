0535f21796d8705015bc7591d1b5bbe521e0a2d9
Shinwoir <raphael.leroy@epita.fr>
2021-04-14 17:34:46 +0200

    some progress, mais ça pue encore bien le caca

---
+++ Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int[] levels;
+    public GameObject buttonPrefab;
+    public Canvas canvas;
+    public void Start()
+    {
+        int i = 0;
+        foreach (int level in levels)
+        {
+            GameObject button2 = Instantiate(buttonPrefab,canvas.transform) as GameObject;
+            Scene scene = SceneManager.GetSceneByBuildIndex(level);
+            if (!scene.IsValid())
+            {
+                Debug.Log("WARNING SCENE IS NULL MDRR");
+            }
+            
+            Debug.Log(scene.name);
+            button2.GetComponentInChildren<TextMeshProUGUI>().text = scene.name;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
