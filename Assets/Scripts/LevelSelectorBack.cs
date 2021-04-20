e4824fdd84457ca883995f077a5adb935cf29b45
Raphaël Leroy <raphael@epita.fr>
2021-04-20 14:08:54 +0200

    back button added, need to work on other side

---
+++ Assets/Scripts/LevelSelectorBack.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Security;
+
+public class LevelSelectorBack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        SceneManager.LoadScene(1);
+    }
+}
