853c24fb8cffd544696782e3db2efac44b1e5359
RedBetta <toan.kriete@gmail.com>
2021-05-31 12:06:38 +0200

    king blob destroys castle with 3 jumps

--- Assets/Scripts/BossAI.cs
+++ Assets/Scripts/BossAI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using Photon.Pun;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = System.Random;
 
 public class BossAI : MonoBehaviour
 {
@@ -27,18 +28,20 @@ public class BossAI : MonoBehaviour
     public GameObject Waypoint2;
     public GameObject Waypoint3;
     private bool phase1 = true;
-    private bool movingToCatsle;
     private bool movingToPhase2;
     private bool phase2;
     private bool movingToPhase3;
     private bool phase3;
     private bool movingToPhase4;
     private bool phase4;
+    private bool isPhase1Playing;
     private bool isMovingToPhase4Playing;
     private bool isPhase4Playing;
     private Coroutine coroutine;
     private Coroutine jumpCoroutine;
     public Tilemap castleTilemap;
+    public Tilemap castleGround;
+    public Grid grid;
 
     [Header("Abilities")] 
     public GameObject throwBlobsParticle;
@@ -46,21 +49,25 @@ public class BossAI : MonoBehaviour
     public GameObject BomberHarpiePrefab;
     public GameObject blobPrefab;
     private float spawnBlob = 0.5f;
-    private float jumpCD = 5f;
+    private float jumpCD = 3f;
+    private float jumpCDStatus;
     private float jumpVelocity = 20f;
+    private float jumpHeight = 10f;
+    private Vector2 jumpDest;
     private bool isJumpPlaying;
     private int nbJumpBeforeDestruction = 3;
     private int nbJump;
+    private bool isJumping;
     private Vector2 pos;
     
     // Start is called before the first frame update
     void Start()
     {
+        jumpCDStatus = jumpCD;
         pos = transform.position;
         rigidbody2d = GetComponent<Rigidbody2D>();
         boxCollider2d = GetComponent<BoxCollider2D>();
-        coroutine = StartCoroutine(Phase1());
-        // jumpCoroutine = StartCoroutine(Jump());
+
     }
     
     public IEnumerator Phase1()
@@ -68,40 +75,58 @@ public class BossAI : MonoBehaviour
         // throw blobs
         yield return new WaitForSeconds(throwBlobsCD);
         PhotonNetwork.Instantiate(throwBlobsParticle.name, transform.position, Quaternion.identity, 1);
+        isPhase1Playing = false;
     }
 
-    public IEnumerator Jump()
-    {
-        yield return new WaitForSeconds(jumpCD);
-        while (Vector2.Distance(transform.position, new Vector2(transform.position.x, pos.y + 10)) > 0)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, new Vector2(transform.position.x, pos.y + 5), speed
-                * Time.deltaTime);
-        }
-        
-    }
-    
     public IEnumerator MovingToPhase4()
     {
         yield return new WaitForSeconds(spawnBlob);
         PhotonNetwork.Instantiate(blobPrefab.name, new Vector2(transform.position.x -4, transform.position.y), Quaternion.identity, 1);
-        
     }
 
     private void Update()
     {
         currentHealth = GetComponent<Enemy>().currentHealth;
         isGrounded = IsGrounded();
-        // Debug.LogFormat("TOUCHING LAYERS : {0}", rigidbody2d.IsTouchingLayers(platformLayerMask));
         Debug.LogFormat("isGrounded : {0}", isGrounded);
 
         if (phase1)
         {
-            if (!isJumpPlaying)
+            if (!isPhase1Playing)
             {
-                nbJump = 0;
-                isJumpPlaying = true;
-                jumpCoroutine = StartCoroutine(Jump());
+                isPhase1Playing = true;
+                coroutine = StartCoroutine(Phase1());
+            }
+            if (jumpCDStatus <= 0 && isGrounded)
+            {
+                // Random rand = new Random();
+                // int n = rand.Next(2);
+                // int x = rand.Next(4);
+                // if (n == 0)
+                // {
+                //     x = -x;
+                // }
+                jumpDest = new Vector2(transform.position.x, transform.position.y + jumpHeight);
+                isJumping = true;
+            }
+            else if (jumpCDStatus > 0 && isGrounded)
+            {
+                jumpCDStatus -= Time.deltaTime;
+            }
+
+            if (isJumping)
+            {
+                if (Vector2.Distance(transform.position, jumpDest) <= 0)
+                {
+                    isJumping = false;
+                    jumpCDStatus = jumpCD;
+                    rigidbody2d.isKinematic = false;
+                }
+                else
+                {
+                    rigidbody2d.isKinematic = true;
+                    transform.position = Vector2.MoveTowards(transform.position, jumpDest, jumpVelocity * Time.deltaTime);
+                }
             }
         }
         
@@ -109,7 +134,7 @@ public class BossAI : MonoBehaviour
         {
             phase1 = false;
             StopCoroutine(coroutine);
-            StopCoroutine(jumpCoroutine);
+            isPhase1Playing = false;
             movingToPhase2 = true;
             // BomberHarpie
             PhotonNetwork.Instantiate(BomberHarpiePrefab.name, new Vector2(-1, 0), Quaternion.identity, 1);
@@ -119,7 +144,9 @@ public class BossAI : MonoBehaviour
         {
             if (Vector2.Distance(transform.position, new Vector2(Waypoint1.transform.position.x, transform.position.y)) <= 0)
             {
-                movingToCatsle = true;
+                movingToPhase2 = false;
+                nbJump = 0;
+                phase2 = true;
             }
             else
             {
@@ -130,28 +157,65 @@ public class BossAI : MonoBehaviour
         
         if (phase2 && nbJump < nbJumpBeforeDestruction)
         {
-            if (!isJumpPlaying)
+            if (jumpCDStatus <= 0 && isGrounded)
             {
-                nbJump = 0;
-                isJumpPlaying = true;
-                jumpCoroutine = StartCoroutine(Jump());
+                jumpDest = new Vector2(transform.position.x, transform.position.y + jumpHeight);
+                isJumping = true;
+            }
+            else if (jumpCDStatus > 0 && isGrounded)
+            {
+                jumpCDStatus -= Time.deltaTime;
+            }
+
+            if (isJumping)
+            {
+                if (Vector2.Distance(transform.position, jumpDest) <= 0)
+                {
+                    Random rand = new Random();
+                    int prob;
+                    isJumping = false;
+                    jumpCDStatus = jumpCD;
+                    rigidbody2d.isKinematic = false;
+                    nbJump += 1;
+                    // Détruire une partie des tiles du château
+                    BoundsInt bounds = castleTilemap.cellBounds;
+                    // TileBase[] allTiles = castleTilemap.GetTilesBlock(bounds);
+                    for (int x = 0; x < bounds.size.x; x++) {
+                        for (int y = 0; y < bounds.size.y; y++) {
+                            Vector3Int coordinate = castleTilemap.WorldToCell(new Vector3(x, y-15, 0));
+                            prob = rand.Next(5);
+                            if (prob == 0)
+                            {
+                                castleTilemap.SetTile(coordinate, null);
+                            }
+                        }
+                    }  
+                }
+                else
+                {
+                    rigidbody2d.isKinematic = true;
+                    transform.position = Vector2.MoveTowards(transform.position, jumpDest, jumpVelocity * Time.deltaTime);
+                    
+                }
             }
         }
-
-        if (phase2 && nbJump >= nbJumpBeforeDestruction)
+        else if (phase2 && nbJump >= nbJumpBeforeDestruction && rigidbody2d.IsTouchingLayers(platformLayerMask))
         {
-            StopCoroutine(jumpCoroutine);
             // On détruit le château
             Destroy(GameObject.Find("Grid").transform.Find(castleTilemap.name).gameObject);
-        }
-        
-        
-
-        if (phase2 && !isGrounded)
-        {
+            Destroy(GameObject.Find("Grid").transform.Find(castleGround.name).gameObject);
+            
             phase2 = false;
             movingToPhase3 = true;
         }
+        
+        
+
+        // if (phase2 && !isGrounded)
+        // {
+        //     phase2 = false;
+        //     movingToPhase3 = true;
+        // }
         
 
         if (movingToPhase3 && isGrounded)
