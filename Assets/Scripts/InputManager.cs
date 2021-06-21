eb913c6baf78da90534c1226c2c7954b5f4eea3a
Raphaël Leroy <raphael.leroy@epita.fr>
2021-06-21 19:23:00 +0200

    aled

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -10,6 +10,9 @@ public class InputManager : MonoBehaviour
     // Start is called before the first frame update
     private static Dictionary<string, KeyCode> actionKeys;
     public GameObject keybindingPrefab;
+    private string actionToRebind;
+    Array kcs = Enum.GetValues(typeof(KeyCode));
+    
     private void Start()
     {
         actionKeys = new Dictionary<string, KeyCode>
@@ -26,9 +29,40 @@ public class InputManager : MonoBehaviour
             GameObject binding = Instantiate(keybindingPrefab, parent.transform);
             binding.transform.Find("Action Text").GetComponent<Text>().text = kvp.Key;
             binding.transform.Find("Button/Key Text").GetComponent<Text>().text = kvp.Value.ToString();
+            Button keyButton = binding.transform.Find("Button").GetComponent<Button>();
+            keyButton.onClick.AddListener(() => StartRebindFor(kvp.Key));
         }
     }
 
+    private void Update()
+    {
+        if (actionToRebind != null)
+        {
+            if (Input.anyKeyDown)
+            {
+                foreach (KeyCode kc in kcs)
+                {
+                    if (Input.GetKeyDown(kc))
+                    {
+                         SetKeyForAction(actionToRebind,kc);
+                         actionToRebind = null;
+                         break;
+                    }
+                }
+            }
+        }
+    }
+
+    void StartRebindFor(string action)
+    {
+        actionToRebind = action;
+    }
+
+    void SetKeyForAction(string action, KeyCode key)
+    {
+        Debug.Log(action + " should now be bound to " + key.ToString());
+        actionKeys[action] = key;
+    }
     public static bool GetKeyDown(string action)
     {
         if (!actionKeys.ContainsKey(action))
