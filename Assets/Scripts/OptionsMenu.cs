92ae3366c7d182af16533de47cb0fae41cde2702
Raphaël Leroy <raphael.leroy@epita.fr>
2021-06-21 14:18:21 +0200

    bruh

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -5,11 +5,17 @@ using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Resolution = UnityEngine.Resolution;
+
 public class OptionsMenu : MonoBehaviour
 {
     public Toggle fullscreenToggle;
     UnityEngine.Resolution[] resolutions;
     public GameObject resOptions;
+    private UnityEngine.Resolution fhd = new UnityEngine.Resolution();
+    private UnityEngine.Resolution hd = new UnityEngine.Resolution();
+     
+
     public void SetRes(int resIndex)
     {
         Screen.SetResolution(resolutions[resIndex].width, resolutions[resIndex].height,Screen.fullScreen);
@@ -29,7 +35,14 @@ public class OptionsMenu : MonoBehaviour
         fullscreenToggle.isOn = Screen.fullScreen;
         //Resolution stuff
         TMP_Dropdown resolutionsDropdown = resOptions.GetComponent<TMP_Dropdown>();
-        resolutions = Screen.resolutions;
+        //resolutions = Screen.resolutions;
+        resolutions = new UnityEngine.Resolution[2];
+        fhd.width = 1920;
+        fhd.height = 1080;
+        hd.width = 1280;
+        hd.height = 720;
+        resolutions[0] = hd;
+        resolutions[1] = fhd;
         resolutionsDropdown.ClearOptions();
         List<string> resStrings = new List<string>();
         int currentResolutionIndex = 0;
