6130d05eaf3d12786625df2cfbe05639a83c4309
Shinwoir <raphael.leroy@epita.fr>
2021-06-22 19:40:31 +0200

    yessir

--- Assets/Scripts/LevelMusic.cs
+++ Assets/Scripts/LevelMusic.cs
@@ -1,14 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelMusic : MonoBehaviour
 {
+    private AudioManager audioManager;
+
+    private int level;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        FindObjectOfType<AudioManager>().StopSound("MenuTheme");
-        FindObjectOfType<AudioManager>().PlaySound("LevelMusic1");
+        audioManager = FindObjectOfType<AudioManager>();
+        
+        audioManager.StopSound("MenuTheme");
+        level = SceneManager.GetActiveScene().buildIndex - 2;
+        if (level >= 1 && level <= 3)
+        {
+            audioManager.PlaySound("LevelMusic1");
+        }
+        else if (level == 6)
+        {
+            audioManager.PlaySound("BossIntro");
+        }
+        else
+        {
+            audioManager.PlaySound("LevelMusic2");
+
+        }
     }
-    
+
+    void Update()
+    {
+        if (level == 6)
+        {
+            if (!audioManager.sounds[5].source.isPlaying)
+            {
+                audioManager.PlaySound("BossLoop");
+                enabled = false;
+            }
+        }
+
+        else
+        {
+            enabled = false;
+        }
+    }
 }
